d25a729e0e38926d0a8c2971bdc1542ec99246a8
Nelson Brandão Carvalho. <jbrandao.72@gmail.com>
2026-01-28 00:52:37 -0300

    Selecionando dados com DAO

--- 1_dao/dao/CarDAO.php
+++ 1_dao/dao/CarDAO.php
@@ -14,6 +14,28 @@ class CarDAO implements CarDAOInterface {
 
     public function findALL() {
 
+        $cars = [];
+
+        $stmt = $this->conn->query("SELECT * FROM cars");
+
+        $data = $stmt->fetchAll();
+
+        foreach($data as $item) {
+
+            $car = new Car();
+
+            $car->setId($item["id"]);
+            $car->setBrand($item["brand"]);
+            $car->setKm($item["km"]);
+            $car->setColor($item["color"]);
+
+            $cars[] = $car;
+
+
+        }
+
+        return $cars;
+
     }
 
     public function create(Car $car) {
--- 1_dao/index.php
+++ 1_dao/index.php
@@ -2,6 +2,11 @@
 
 
     include_once("db.php");
+    include_once("dao/CarDAO.php");
+
+    $carDAO = new CarDAO($conn);
+
+    $cars = $carDAO->findAll();
 
 ?>
 
@@ -24,3 +29,8 @@
     <br>
     <input type="submit" value="Salvar">
 </form>
+<ul>
+    <?php foreach($cars as $car): ?>
+        <li><?= $car->getBrand() ?> - <?= $car->getKm() ?> - <?= $car->getColor() ?></li>
+    <?php endforeach; ?>
+</ul>
